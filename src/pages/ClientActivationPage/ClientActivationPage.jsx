d198f2604878a793dd75a9a3c833bbb35da8b55d
asgoDev <asgodev@gmail.com>
2024-10-01 23:27:38 -0400

    Reglas de validacion establecidas

--- src/pages/ClientActivationPage/ClientActivationPage.jsx
+++ src/pages/ClientActivationPage/ClientActivationPage.jsx
@@ -8,19 +8,69 @@ const formConfig = [
     type: 'date',
     defaultValue: new Date().toLocaleDateString('en-CA'),
   },
-  { label: 'Número de PON', name: 'ponNumber' },
+  {
+    label: 'Número de PON',
+    name: 'ponNumber',
+    params: {
+      pattern: {
+        value: /^[a-fA-F0-9]{24}$/,
+        message: 'Dato inválido',
+      },
+    },
+  },
   {
     label: 'Cédula',
     name: 'idCard',
     type: 'number',
     params: {
-      maxLength: { value: 6, message: 'Nro. de cédula inválido' },
+      minLength: { value: 6, message: 'Nro. de cédula inválido' },
+      maxLength: { value: 7, message: 'Nro. de cédula inválido' },
+      pattern: {
+        value: /^[0-9]+$/,
+        message: 'Este campo solo admite números',
+      },
     },
   },
-  { label: 'Apellidos', name: 'lastName' },
-  { label: 'Nombres', name: 'firstName' },
-  { label: 'Correo', name: 'email' },
-  { label: 'Teléfono', name: 'phone' },
+  {
+    label: 'Apellidos',
+    name: 'lastName',
+    params: {
+      pattern: {
+        value: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ' ]+$ /,
+        message: 'Este campo solo admite letras',
+      },
+    },
+  },
+  {
+    label: 'Nombres',
+    name: 'firstName',
+    params: {
+      pattern: {
+        value: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ' ]+$ /,
+        message: 'Este campo solo admite letras',
+      },
+    },
+  },
+  {
+    label: 'Correo',
+    name: 'email',
+    params: {
+      pattern: {
+        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        message: 'Correo inválido',
+      },
+    },
+  },
+  {
+    label: 'Teléfono',
+    name: 'phone',
+    params: {
+      pattern: {
+        value: /^\d{11}$/,
+        message: 'Número de teléfono inválido',
+      },
+    },
+  },
   { label: 'MDT', name: 'mdt' },
   {
     label: 'Dirección',
@@ -31,8 +81,28 @@ const formConfig = [
   },
   { label: 'Coordenadas de la Casa', name: 'houseCoordinates' },
   { label: 'Coordenadas del NAP', name: 'napCoordinates' },
-  { label: 'Cuota inicial del drop', name: 'initialDropQuota', type: 'number' },
-  { label: 'Cuota final del drop', name: 'finalDropQuota', type: 'number' },
+  {
+    label: 'Cuota inicial del drop',
+    name: 'initialDropQuota',
+    type: 'number',
+    params: {
+      pattern: {
+        value: /^[0-9]+$/,
+        message: 'Este campo solo admite números',
+      },
+    },
+  },
+  {
+    label: 'Cuota final del drop',
+    name: 'finalDropQuota',
+    type: 'number',
+    params: {
+      pattern: {
+        value: /^[0-9]+$/,
+        message: 'Este campo solo admite números',
+      },
+    },
+  },
 ]
 const ClientActivationPage = () => {
   return (
